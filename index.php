888883a9a3ab456721b60eddcfd3f4d7c8d706e1
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-01 13:47:14 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 /** User: Paprika...**/
 
+require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
+
 $app = new Application();
+
+$app->router->get('/', function(){
+    return 'Hello World'; 
+});
+
+$app->userRouter($router);
 
 echo "Hello World";
