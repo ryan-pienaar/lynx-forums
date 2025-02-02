058d7a33aafbbe7defc6739b419aba2640930b9e
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-02 17:17:25 +0000

    Using LynxController

--- public/index.php
+++ public/index.php
@@ -4,14 +4,15 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-
+use app\controllers\LynxController;
 use app\core\Application;
 
 $app = new Application(dirname(__DIR__));
 
 
-$app->router->get('/', 'home');
-$app->router->get('/contact', 'contact');
+$app->router->get('/', [LynxController::class, 'home']);
+$app->router->get('/contact', [LynxController::class, 'contact']);
+$app->router->post('/contact', [LynxController::class, 'handleContactData']);
 
 
 $app->run();
