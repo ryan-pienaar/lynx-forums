b01f433a91bc57d86d5e967e8150cc9680fc68ec
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-02 15:39:26 +0000

    Addition of Response core

--- core/Router.php
+++ core/Router.php
@@ -10,14 +10,17 @@ namespace app\core;
 
 class Router {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
     /**
      * @param Request $request
+     * @param Response $response
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -31,6 +34,7 @@ class Router {
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
+            $this->response->setStatusCode(404);
             return "Not Found";
         }
         if (is_string($callback)) {
@@ -41,6 +45,22 @@ class Router {
 
     public function renderView($view)
     {
-        include_once __DIR__ . "/../views/$view.php";
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,9 +2,12 @@
 /** User: Paprika...**/
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+
+
 use app\core\Application;
 
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
 
 $app->router->get('/', 'home');
