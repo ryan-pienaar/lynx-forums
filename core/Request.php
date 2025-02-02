5f4a74371d8c4758e249fc5d79a334f753fb95ad
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-02 18:17:48 +0000

    Sanitiiiize init

--- core/Request.php
+++ core/Request.php
@@ -28,4 +28,18 @@ class Request {
     {
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
+
+    public function getBody()
+    {
+        $body = [];
+
+        //Character sanitization - Security
+        if ($this->getMethod() === 'get') {
+            foreach ($_GET as $key => $value) {
+                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        return $body;
+    }
 }
