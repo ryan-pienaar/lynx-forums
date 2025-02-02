9060916879f6e7761bf4068c6ab0264ed6ec5e27
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-02 17:17:10 +0000

    Contact form

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,17 @@
 <h1>Contact</h1>
+
+<form action="" method="post">
+    <div class="mb-3">
+        <label  class="form-label">Subject</label>
+        <input type="text" class="form-control" name="subject">
+    </div>
+    <div class="mb-3">
+        <label  class="form-label">Email</label>
+        <input type="text" class="form-control" name="email">
+    </div>
+    <div class="mb-3">
+        <label  class="form-label">Body</label>
+        <textarea class="form-control" name="body"> </textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
