d7cf8128ec4d3cc2d7da8f91a5efc44ef5a0e71f
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-03 20:43:38 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $conf = [
+    'userClass' => \app\models\User::class,
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'user' => $_ENV['DB_USER'],
