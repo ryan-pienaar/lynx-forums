a362d0a8fb8eac1926e43ca8cd9d2f413fdcd130
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-03 18:56:43 +0000

    labels

--- models/User.php
+++ models/User.php
@@ -48,4 +48,16 @@ class User extends DBModel
     {
         return ['username', 'firstName', 'lastName', 'email', 'password', 'status'];
     }
+
+    public function labels(): array
+    {
+        return [
+            'username' => 'Username',
+            'firstName' => 'First Name',
+            'lastName' => 'Last Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'confirmPassword' => 'Confirm Password'
+        ];
+    }
 }
