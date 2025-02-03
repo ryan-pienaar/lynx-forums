3ad5e422a0f285b134d3715c86967559d1c40b61
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-03 20:43:43 +0000

    Update login.php

--- views/login.php
+++ views/login.php
@@ -1,1 +1,15 @@
+<?php
+
+/** @var $model app\models\User */
+
+?>
+
 <h1>Login</h1>
+
+<?php $form = \app\core\form\Form::begin('', "post") ?>
+
+<?php echo $form->field($model, 'email') ?>
+<?php echo $form->field($model, 'password')->passwordField() ?>
+
+<button type="submit" class="btn btn-primary">Login</button>
+<?php \app\core\form\Form::end() ?>
