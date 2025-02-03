29909b75562e8cef833f976d25be7205554638d7
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-03 20:42:09 +0000

    login functions

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -5,11 +5,13 @@ namespace app\controllers;
 use app\core\Controller;
 use app\core\Kernel;
 use app\core\Request;
+use app\core\Response;
+use app\models\LoginForm;
 use app\models\User;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(Request $request ,Response $response)
     {
 
         $user = new User();
@@ -18,7 +20,7 @@ class AuthController extends Controller
 
             if ($user->validate() && $user->save()) {
                 Kernel::$kernel->session->setFlash('success', 'Thanks for registering!');
-                Kernel::$kernel->response->redirect('/');
+                $response->redirect('/');
             }
             return $this->render('register', [
                'model' => $user
@@ -31,9 +33,20 @@ class AuthController extends Controller
         ]);
     }
 
-    public function login(Request $request)
+    public function login(Request $request, Response $response)
     {
+        $loginForm = new LoginForm();
+        if ($request->isPost()) {
+            $loginForm->loadData($request->getBody());
+
+            if ($loginForm->validate() && $loginForm->login()) {
+                //Kernel::$kernel->session->setFlash('success', 'Thanks for logging in!');
+                $response->redirect('/');
+            }
+        }
         $this->setLayout('auth');
-        return $this->render('login');
+        return $this->render('login', [
+            'model' => $loginForm
+        ]);
     }
 }
