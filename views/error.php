a75ad3bc8bb9a9f150dd827e3eebdd0786b6501f
tempestdev <93183950+tempestdev-exe@users.noreply.github.com>
2025-02-03 23:27:22 +0000

    Updates people updates!

--- views/error.php
+++ views/error.php
@@ -1,12 +1,18 @@
 <?php
+
+/** @var $exception \Exception */
+/** @var $this \app\core\View */
+
+$this->title = "Error | " . $exception->getCode();
+
 ?>
 
 <div class="page-wrap d-flex flex-row align-items-center">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12 text-center">
-                <span class="display-1 d-block">404</span>
-                <div class="mb-4 lead">The page you are looking for was not found.</div>
+                <span class="display-1 d-block"><?php echo $exception->getCode() ?></span>
+                <div class="mb-4 lead"><?php echo $exception->getMessage() ?></div>
                 <a href="/" class="btn btn-link">Back to Home</a>
             </div>
         </div>
